e7289a9dac22a426974e2034439e04b39ec62224
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-02-16 16:59:38 +0000

    express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+import * as express from "express";
+import { message } from "../index1";
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(message)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
