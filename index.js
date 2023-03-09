b10926ada8fc1d2d7df3803bf5d0dac5ab85e6fc
NoahF721 <111761297+NoahF721@users.noreply.github.com>
2023-03-09 16:30:30 +0000

    Comments

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // Variables
-// const = immutable (can not be changed)
-// let = mutable (can be changed)
 const x = "x"
 let y = "y"
 
 // Functions
 function func1 () {
-  console.log("Hello"); console.log("Hello 2")
+  console.log("Hello"); 
 }
 
 const func2 = () => {
-  console.log("Hello")
+  console.log("Hello");
 }
 
+// Function calls
 func1()
 func2()
 
